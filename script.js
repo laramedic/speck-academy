6b763a4d2b0dac6dc7feb4b1a460757f67004322
Lara Medić <72031668+laramedic@users.noreply.github.com>
2021-02-18 21:15:27 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+<script type='text/javascript'>
+      function addProduct() {
+        var ul = document.getElementById("lista");
+        var unos = document.getElementById("unos");
+        var li = document.createElement("li");
+        li.setAttribute('id', unos.value);
+        li.appendChild(document.createTextNode(unos.value));
+        ul.appendChild(li);
+      }
+
+      function removeProducts() {
+        var ul = document.getElementById("lista");
+        var unos = document.getElementById("unos");
+        var item = document.getElementById(unos.value);
+        ul.removeChild(item);
+      }
+
+      function countItems() {
+        var ul = document.getElementById("unos");
+        var i = 0,
+          itemCount = 0;
+        while (ul.getElementsByTagName('li')[i++]) itemCount++;
+        document.write(itemCount);
+      }
+
+</script>
