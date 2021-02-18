22ca4aa2e518044ee3bfec4c46e43f29551ddeec
Lara Medić <72031668+laramedic@users.noreply.github.com>
2021-02-18 23:00:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-<script type='text/javascript'>
-      function addProduct() {
-        var ul = document.getElementById("lista");
-        var unos = document.getElementById("unos");
-        var li = document.createElement("li");
-        li.setAttribute('id', unos.value);
-        li.appendChild(document.createTextNode(unos.value));
-        ul.appendChild(li);
-      }
+function addProduct() {
+  var ul = document.getElementById("lista");
+  var unos = document.getElementById("unos");
+  var li = document.createElement("li");
+  li.setAttribute("id", unos.value);
+  li.appendChild(document.createTextNode(unos.value));
+  ul.appendChild(li);
+}
 
-      function removeProducts() {
-        var ul = document.getElementById("lista");
-        var unos = document.getElementById("unos");
-        var item = document.getElementById(unos.value);
-        ul.removeChild(item);
-      }
+function removeProduct() {
+  var ul = document.getElementById("lista");
+  var unos = document.getElementById("unos");
+  var item = document.getElementById(unos.value);
+  ul.removeChild(item);
+}
 
-      function countItems() {
-        var ul = document.getElementById("unos");
-        var i = 0;
-        itemCount = 0;
-        while (ul.getElementsByTagName('li')[i++]) itemCount++;
-        document.write(itemCount);
-      }
-</script>
+function countItem() {
+  var ul = document.getElementById("unos");
+  var i = 0;
+  itemCount = 0;
+  while (ul.getElementsByTagName("li")[i++]) itemCount++;
+  document.write(itemCount);
+}
